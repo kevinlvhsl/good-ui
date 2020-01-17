b6c2e113bd9469e862ac4e9559b7ff17e49656c6
kevinlvhsl <kevinlvhsl@users.noreply.github.com>
2020-01-17 14:10:37 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 window.Vue = Vue;
 import App from './App.vue'
-import Button from '../packages/buttons';
+import Button from '../packages/button';
 Vue.config.productionTip = false
 Vue.use(Button);
 new Vue({
